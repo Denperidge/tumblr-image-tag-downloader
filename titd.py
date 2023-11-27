8f23a6b1bbd916059dbb9249af16718206a4b4dd
Denperidge <denperidge@gmail.com>
2023-11-27 14:10:50 +0100

    Refactor into functions

--- titd.py
+++ titd.py
@@ -29,58 +29,76 @@ def setup_cli():
     argparse.add_argument("--tag", "-t", required=True, help="Tag to download")
     return argparse.parse_args()
 
+def create_request_url(blogname: str, api_key: str, tag: str):
+    return "https://api.tumblr.com/v2/blog/" \
+        + blogname \
+        + "/posts" \
+        + "?api_key=" + api_key \
+        + "&tag=" + tag
+
+def get_posts(blogname: str, api_key: str, tag: str):
+    request_url = create_request_url(blogname, api_key, tag)
+    print(f"[TITD] Sending request to {request_url}")
+    with urlopen(request_url) as req:
+        posts = loads(req.read())["response"]["posts"]
+    return posts 
 
 
+def parse_post(post_object):
+    # Get the short_url to link back
+    post_url: str = post_object["post_url"]
+    # Get the post with all reblogs to extract the image & image id
+    trail: str = post_object["trail"]
+
+    print(f"[TITD] Processing {post_url}")
+
+    post_images = []
+
+    # All images in any parts of the post
+    post_image_urls = []
+    # All image descriptiosn in any parts of the post
+    post_image_descriptions = []
+
+    # Process every part
+    for post_part in trail:
+        post_part_content = post_part["content"]
+
+        post_image_urls += REGEX_IMAGES.findall(post_part_content)
+        post_image_descriptions += REGEX_IMAGE_DESCRIPTION.findall(post_part_content)
+    
+    # After finding all image urls & description, transform that data into a json object & add it to post_images
+    for i in range(len(post_image_urls)):
+        image = dict({
+            "image_url": post_image_urls[i],
+            "post_url": post_url
+        })
+        try:
+            image["alt"] = post_image_descriptions[i]
+        except IndexError:
+           print("No alt text for " + image["image_url"])
+        
+        post_images.append(image)
+    
+    # Return post_images
+    return post_images
+
+def save_output_json(filename: str, images: list):
+    with open(filename, "w", encoding="UTF-8") as output:
+        print(f"[TITD] Saving output...")
+        dump(images, output)
+    
 # Main
 if __name__ == "__main__":
     args = setup_cli()
 
-    request_url = "https://api.tumblr.com/v2/blog/" \
-        + args.blog \
-        + "/posts" \
-        + "?api_key=" + args.api_key \
-        + "&tag=" + args.tag
-        
-    print(f"[TITD] Sending request to {request_url}")
+    all_post_objects = get_posts(args.blog, args.api_key, args.tag)
 
-    with urlopen(request_url) as req:
-        all_posts = loads(req.read())["response"]["posts"]
-    
     images = []
 
-    for post in all_posts:
-        # Get the short_url to link back
-        post_url: str = post["post_url"]
-        # Get the post with all reblogs to extract the image & image id
-        trail: str = post["trail"]
+    for post_object in all_post_objects:
+        images += parse_post(post_object)
 
-        post_images = []
-        post_image_descriptions = []
+    save_output_json("output.json", images)
 
-        print(f"[TITD] Processing {post_url}")
-
-        for post_part in trail:
-            post_part_content = post_part["content"]
-
-            post_images += REGEX_IMAGES.findall(post_part_content)
-            post_image_descriptions += REGEX_IMAGE_DESCRIPTION.findall(post_part_content)
-
-
-        for i in range(len(post_images)):
-            image = dict({
-                "image_url": post_images[i],
-                "post_url": post_url
-            })
-            try:
-                image["alt"] = post_image_descriptions[i]
-            except IndexError:
-                print("No alt text for " + image["image_url"])
-
-            images.append(image)
-        
-    with open("output.json", "w") as output:
-        print(f"[TITD] Saving output...")
-        dump(images, output)
 
     print(f"[TITD] Done!")
-    
